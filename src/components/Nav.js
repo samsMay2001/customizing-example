04bbda7ece5bbc4e0a547bc9f04d196e77eae454
sam <samscloud2@gmail.com>
2023-03-15 15:13:43 -0300

    components modification

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+function Nav(){
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li>Home</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
